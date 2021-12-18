81d1eb4bbb4ca5b8e4b7623c77688615240e6e7d
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2021-12-18 17:07:32 +0100

    Esercizio Palindroma.

--- palindroma/myscript.js
+++ palindroma/myscript.js
@@ -4,13 +4,26 @@
 
 //Svolgimento : 
 
-//Creo una funzione per vericare se la Parola inserita è Palindroma
-function palindCheck (parolaUser){
-
-
-
-
+function checkPalindrome(word) { //funzione che stabilisce se la parola scelta è Palindroma.
+    let l = wordArr.length;
+    for (let i = 0; i < l/2 ; i++) {
+        if (wordArr[i] == wordArr[l-1-i]){ // Confronto fra di loro i diversi indici dell'array.
+            return true;
+        }
+    }
+    return false;
 }
 
+let word = prompt('inserisci una parola');//Parola scelta dall'utente.
+let wordArr = [];//creo array vuoto. 
 
-let parolaUser = prompt('Inserisci un Parola');// Parola inserita dall'utente
+for(i=0; i<word.length; i++){   /* inserisco nell'array i singoli elementi che compongono 
+                                la parola scelta dall'utente.*/
+    wordArr.push(word[i]);
+}
+
+if (checkPalindrome(word)) { //Se la Funzione restituisce il volore true : la parola è Palindroma.
+    document.write("La Parola è Palindroma");
+} else { //Altrimenti la Funzione restituisce il volore false : la parola NON è Palindroma.
+    document.write("La Parola non è Palindroma");
+}
--- pari.dispari/myscript.js
+++ pari.dispari/myscript.js
@@ -7,7 +7,7 @@
 
 //Svolgimento:
 
-//Creo Funzione per fare la somma di numeroUtente e numeroPc
+//Creo Funzione per fare la somma di numeroUtente e numeroPc.
 function SommaNum(numUser, numPc){
 
     let risultato = numUser + numPc;
@@ -15,13 +15,14 @@ function SommaNum(numUser, numPc){
     return risultato; 
 }
 
-let parolaUser = prompt('Pari o Dispari?');//chiedo all'utente di inserire la parola Pari o Dispari
-let numUser;//creo variabile numero Utente, non definita
-let numPc;//creo variabile numero PC, non definita
-let somma;// creo variabile per assegnare risultato della Funzione, non definita
+let parolaUser = prompt('Pari o Dispari?');//chiedo all'utente di inserire la parola Pari o Dispari.
+let numUser;//creo variabile numero Utente, non definita.
+let numPc;//creo variabile numero PC, non definita.
+let somma;// creo variabile per assegnare risultato della Funzione, non definita.
 
 
-if((parolaUser == 'Pari')||(parolaUser =='Dispari')){ //condizione necessaria affinchè le variabili non definite vengano generate
+if((parolaUser == 'Pari')||(parolaUser =='Dispari')){ //condizione necessaria affinchè le variabili non definite vengano generate.
+    
     numUser = parseInt(prompt('Scegli un numero compreso tra 1 e 5'));
     numPc = Math.floor(Math.random()*5 + 1);
     somma = SommaNum(numUser, numPc);
@@ -30,16 +31,16 @@ if((parolaUser == 'Pari')||(parolaUser =='Dispari')){ //condizione necessaria af
     console.log(numPc);
     console.log(somma);
     
-    //Svolgimento del gioco 
-    if((parolaUser == 'Pari')&&(somma%2 == 0)){ // Condinzione Vittoria Pari
+    //Svolgimento del gioco.
+    if((parolaUser == 'Pari')&&(somma%2 == 0)){ // Condinzione Vittoria Utente Pari.
         
         console.log('HAI VINTO!');
     
-    }else if((parolaUser == 'Dispari')&&(somma%2 != 0)){ // Condinzione Vittoria Dispari
+    }else if((parolaUser == 'Dispari')&&(somma%2 != 0)){ // Condinzione Vittoria Utente Dispari.
         
         console.log('HAI VINTO');
     
-    }else{ // Condizione Sconfitta
+    }else{ // Condizione Sconfitta Utente e Vittoria del Computer.
 
         console.log('HAI PERSO');
     }
@@ -52,4 +53,4 @@ if((parolaUser == 'Pari')||(parolaUser =='Dispari')){ //condizione necessaria af
 
 
 
-    
+
