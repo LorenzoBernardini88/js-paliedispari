5f53705f2106e045f14dc66e66b18e5401231d99
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2021-12-19 00:31:56 +0100

    Modifico Svolgimento pari-dispari

--- pari.dispari/myscript.js
+++ pari.dispari/myscript.js
@@ -8,8 +8,9 @@
 //Svolgimento:
 
 //Creo Funzione per fare la somma di numeroUtente e numeroPc.
-function SommaNum(numUser, numPc){
+function SommaNum(){
 
+    numRandom = Math.floor(Math.random()*5 + 1);
     let risultato = numUser + numPc;
 
     return risultato; 
@@ -17,34 +18,42 @@ function SommaNum(numUser, numPc){
 
 let parolaUser = prompt('Pari o Dispari?');//chiedo all'utente di inserire la parola Pari o Dispari.
 let numUser;//creo variabile numero Utente, non definita.
-let numPc;//creo variabile numero PC, non definita.
+let numComp;//creo variabile numero PC, non definita.
 let somma;// creo variabile per assegnare risultato della Funzione, non definita.
 
 
 if((parolaUser == 'Pari')||(parolaUser =='Dispari')){ //condizione necessaria affinchè le variabili non definite vengano generate.
     
     numUser = parseInt(prompt('Scegli un numero compreso tra 1 e 5'));
-    numPc = Math.floor(Math.random()*5 + 1);
-    somma = SommaNum(numUser, numPc);
 
-    console.log(numUser);
-    console.log(numPc);
-    console.log(somma);
-    
-    //Svolgimento del gioco.
-    if((parolaUser == 'Pari')&&(somma%2 == 0)){ // Condinzione Vittoria Utente Pari.
+    if((numUser>=1)&&(numUser<=5)){
         
-        console.log('HAI VINTO!');
-    
-    }else if((parolaUser == 'Dispari')&&(somma%2 != 0)){ // Condinzione Vittoria Utente Dispari.
+        somma = SommaNum();
+        numComp = numRandom;
+        console.log(numUser);
+        console.log(numComp);
+        console.log(somma);
         
-        console.log('HAI VINTO');
-    
-    }else{ // Condizione Sconfitta Utente e Vittoria del Computer.
+        //Svolgimento del gioco.
+        if((parolaUser == 'Pari')&&(somma%2 == 0)){ // Condinzione Vittoria Utente Pari.
+            
+            console.log('HAI VINTO!');
+        
+        }else if((parolaUser == 'Dispari')&&(somma%2 != 0)){ // Condinzione Vittoria Utente Dispari.
+            
+            console.log('HAI VINTO');
+        
+        }else{ // Condizione Sconfitta Utente e Vittoria del Computer.
 
-        console.log('HAI PERSO');
+            console.log('HAI PERSO');
+        }
+
+    }else {
+
+        console.log('ERRORE!!! INSERISCI un Numero compreso tra 1 e 5'); // Messaggio Errore per Condizione iniziale non verificata.
+        
     }
-        
+    
 }else {
     console.log('ERRORE!!! INSERISCI "Pari" o "Dispari"'); // Messaggio Errore per Condizione iniziale non verificata.
 }
